94c33ebdd7778d9322382804aaa8c84147b59de9
Jadenayo <108669742+Jadenayo@users.noreply.github.com>
2025-04-17 12:01:40 -0400

    Made Hp_system
    Added signal "Damage to fly"
    Gave the fly a damage collision
    added hp bar to player

--- synthetic-awakening/Scenes/penemy_fly.gd
+++ synthetic-awakening/Scenes/penemy_fly.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var speed = 40
 var player_chase = false
 var player = null
+signal Damage
 
 func _physics_process(delta):
 	if player_chase == true:
@@ -19,3 +20,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	var player_chase = false
 	var player = null
 	speed = 9999999999999999999
+
+
+func _on_damage_collision_body_entered(body: Node2D) -> void:
+	emit_signal("Damage")
+	pass # Replace with function body.
