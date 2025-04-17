f1a580adfa1fd2cbe088ca9133c3531f5532a404
Okaierac <sepdavid@hotmail.com>
2025-04-17 11:38:59 -0400

    Finished "Penemy_Knight"

--- synthetic-awakening/Enemies and traps/penemy_knight.gd
+++ synthetic-awakening/Enemies and traps/penemy_knight.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
-var speed = 40
+var speed = 300.0
 var player_detected = false
 var player_chase = false
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _on_penemy_fly_player_seen() -> void:
 	player_detected = true
 
 func _physics_process(delta: float) -> void:
-	if player_detected:
-		position += ($"../Player_Side".position - position)/speed
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	# Chase logic
+	if player_detected:
+		var target_position = $"../Player_Side".position
+		var direction = (target_position - position).normalized()
+		velocity.x = direction.x * speed  # Update velocity based on direction to chase the player
+		move_and_slide()
+
+	# Flip sprite direction based on velocity.x
+	if velocity.x > 0:
+		sprite_2d.flip_h = true  # Moving right, no flip
+	elif velocity.x < 0:
+		sprite_2d.flip_h = false   # Moving left, flip
