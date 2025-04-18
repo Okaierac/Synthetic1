763bc89e4eb67c9ef9c17957c563c3d25ffe2c1a
Okaierac <sepdavid@hotmail.com>
2025-04-17 21:59:37 -0400

    merging main...

--- synthetic-awakening/Items/Change_Scene_(Item).gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var top_down_scene: String = "res://Scenes/topdown_main.tscn" # Path to the top-down scene
-@onready var sprite_2d: Sprite2D = $Sprite2D
-
-func _on_body_entered(body):
-	print("Touched by:", body.name)  # Debugging
-	if body.is_in_group("player_side"):
-		get_tree().change_scene_to_file(top_down_scene)
