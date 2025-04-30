f89eb7c5c14fef198aabe40d62f050b67db06c30
Jadenayo <108669742+Jadenayo@users.noreply.github.com>
2025-04-30 11:38:23 -0400

    Made neater and mainmenu exit

--- Parallax/Main_Menu/main_menu.gd
+++ Parallax/Main_Menu/main_menu.gd
@@ -13,3 +13,8 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/topdown_main.tscn")
+
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
