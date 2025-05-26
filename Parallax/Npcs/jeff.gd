9fe532445351ecc14c7ffe3a29c9814cd5e61b83
Okaierac <sepdavid@hotmail.com>
2025-05-26 12:00:24 -0400

    did smth

--- Parallax/Npcs/jeff.gd
+++ Parallax/Npcs/jeff.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 var ready_to_tp := false
 @onready var interactable: Area2D = $Interactable
 @onready var label: Label = $Label
-signal side_available
 
 
 func _ready() -> void:
@@ -12,7 +11,6 @@ func _on_interact():
 	DialogueManager.show_example_dialogue_balloon(load("res://dialogues/tp.dialogue"), "start")
 	var ready_to_tp = true
 	if ready_to_tp == true:
-		side_available.emit()
 		get_tree().change_scene_to_file("res://Scenes/side_main.tscn")
 	return
 
